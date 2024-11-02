d07a413544d1588969e9dcf50e08faa70c4a0fda
Hung Thuan Nguyen <hungthuan.dev@gmail.com>
2024-11-02 11:22:10 +0700

    validate env variable

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,11 @@
 import 'dotenv/config'
 import App from "./app";
 import { IndexRoute } from "./modules/index";
+import { validateEnv } from './core/utils';
+
+validateEnv();
 
 const routes = [new IndexRoute()];
-
 
 const app = new App(routes);
 
