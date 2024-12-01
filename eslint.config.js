0fb5301e447fde91818c724ca8a67f04642977b5
Hung Thuan Nguyen <hungthuan.dev@gmail.com>
2024-12-01 15:48:23 +0700

    update config eslint

--- eslint.config.js
+++ eslint.config.js
@@ -3,7 +3,7 @@ import pluginJs from '@eslint/js'
 import tseslint from 'typescript-eslint'
 
 export default [
-  { files: ['**/*.{js,mjs,cjs,ts}'] },
+  { files: ['**/*.{js,mjs,ts}'] },
   { languageOptions: { globals: globals.node } },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
@@ -13,6 +13,6 @@ export default [
       '@typescript-eslint/no-unused-vars': 'off',
       'no-console': 'error'
     },
-    ignores: ['**/node_modules/', '**/dist/']
+    ignores: ['**/node_modules/', '**/dist/server.cjs']
   }
 ]
